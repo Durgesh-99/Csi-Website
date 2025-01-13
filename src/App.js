e2481b6a196abb589f48e253691ed659b12c94eb
Durgesh <durgeshsonarjee@gmail.com>
2025-01-13 20:33:40 +0530

    added redirects

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
             <Route path='/about' element={<About />} />
             <Route path='/events' element={<Events />} />
             <Route path='/contact' element={<Contact />} />
+            <Route path='*' element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </div>
